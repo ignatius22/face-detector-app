da2cb626f315958d472301104437bded4cac027a
Ignatius22 <igee54077@gmail.com>
2020-06-04 17:25:40 +0100

    add features for particle.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
 import Navigation from './components/navigation/navigation';
-import './App.css'
+import './App.css';
 import Logo from './components/logo/logo';
-import ImageLinkForm  from './components/ImageLinkForm /ImageLinkForm '
-import Rank from './components/Rank/Rank'
+import ImageLinkForm from './components/ImageLinkForm /ImageLinkForm ';
+import Particles from 'react-particles-js';
+import Rank from './components/Rank/Rank';
+
+
+
+
+const particlesOption = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 250
+      }
+    }
+  }
+}
+
 
 export class App extends Component {
   render() {
+  
     return (
-      <div className='App'>
+      <div className="App">
+        <Particles className="particles" params={particlesOption} />
+
         <Navigation />
         <Logo />
         <Rank />
         <ImageLinkForm />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
