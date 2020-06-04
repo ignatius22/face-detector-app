532890dda329fb98839211ca78d8b867116277e1
Ignatius22 <igee54077@gmail.com>
2020-06-04 01:40:33 +0100

    add particles.js npm

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import Navigation from './components/navigation/navigation';
+import './App.css'
 import Logo from './components/logo/logo';
 import ImageLinkForm  from './components/ImageLinkForm /ImageLinkForm '
+import Rank from './components/Rank/Rank'
 
 export class App extends Component {
   render() {
     return (
-      <div>
+      <div className='App'>
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
       </div>
     )
--- src/components/ImageLinkForm /ImageLinkForm .js
+++ src/components/ImageLinkForm /ImageLinkForm .js
@@ -1,14 +1,20 @@
 import React from 'react'
+import './ImageLinkForm.css'
+
 
 const ImageLinkForm =  () =>{
 	return (
 		<div>
 			<p className='f3'>
-				{'this magic brain will detect face in your pictures. Give it a try'}
+				{ 'this magic brain will detect face in your pictures. Give it a try' }
 			</p>
 			<div className='center'>
-			<input className='f4 pa3 w-70 center' type='text' />
-			<button className='w-30 grow f4 ph3 pv2 dib white bg-light-purple'>Detect</button>
+
+					<div className='form center pa4 br3 shadow-5'>
+						<input className='f4 pa3 w-70 center' type='text' />
+						<button className='w-30 grow f4 ph3 pv2 dib white bg-light-purple'>Detect</button>
+					</div>
+		
 			</div>
 		</div>
 	)
