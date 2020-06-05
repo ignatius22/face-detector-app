9141c94c7478150b486dfb57d9790664abb8bc2c
Ignatius22 <igee54077@gmail.com>
2020-06-05 05:37:47 +0100

    add facerecognition css file

--- src/components/Facerecognition/Facerecognition.js
+++ src/components/Facerecognition/Facerecognition.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import './Facerecognition.css'
 
-const  Facerecognition = ({imageUrl}) =>{
+const  Facerecognition = ({imageUrl, box}) =>{
 	return (
 		<div className='center ma'>
 			<div className='absolute mt2'>
-			<img alt='face' src={imageUrl}  width='500' height='auto'/>
+			<img id='input-image' alt='face' src={imageUrl}  width='500' height='auto'/>
+			<div className='bounding-box' style={{top:box.topRow, right:box.rightCol, bottom:box.bottomRow, left:box.leftCol }}></div>
 			</div>
 		</div>
 	)
